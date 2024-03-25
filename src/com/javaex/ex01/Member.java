71f95d0ae2d78acac5459c1327f76fb65b4032d8
JJiwonn <kimjiwon4186@gmail.com>
2024-03-25 19:04:28 +0900

    프렉티스240325

---
+++ src/com/javaex/ex01/Member.java
@@ -0,0 +1,35 @@
+package com.javaex.ex01;
+
+public class Member {
+      private String id;
+      private String name;
+      private int point;
+      
+      public Member( String id, String name, int point) {
+    	  this.id = id;
+    	  this.name = name;
+    	  this.point = point;
+      }
+      public String getId() {
+    	  return id;
+      }
+      public void setId(String id) {
+    	  this.id = id;
+      }
+      public String getName() {
+    	  return name;
+      }
+      public void setName(String name) {
+    	  this.name = name;
+      }
+      public int getPoint() {
+    	  return point;
+      }
+      public void getPoint(int point) {
+    	  this.point = point;
+      }
+      public void showInfo() {
+		  System.out.printf("%s: %s, %d점)%n",
+				  id, name, point);
+	  }
+}
